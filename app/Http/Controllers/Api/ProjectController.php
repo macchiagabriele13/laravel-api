156201f9f531a26552c2f31e89b29d9834d5c4c4
macchiagabriele13 <macchia_gabriele@libero.it>
2023-01-19 15:54:54 +0100

    Commit- Modifiche ProjectController API

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'success' => true,
+            'results' => Project::with(['technologies', 'type'])->orderByDesc('id')->paginate(5)
+        ]);
+
+        //return Post::orderByDesc('id')->paginate(5);
+    }
 }
