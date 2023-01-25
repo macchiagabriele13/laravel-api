467cc5bc8fbd0f2af3baaad35375e330a8640122
macchiagabriele13 <macchia_gabriele@libero.it>
2023-01-25 16:20:12 +0100

    Commit - Add Email

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Support\Facades\Validator;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Lead;
+use App\Mail\NewContact;
+use Illuminate\Support\Facades\Mail;
+
+class LeadController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
+
+        Mail::to('admin@google.com')->send(new NewContact($new_lead));
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Project;
@@ -23,3 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/projects', [ProjectController::class, 'index']);
 
 Route::get('/projects/{project:slug}', [ProjectController::class, 'show']);
+
+Route::post('/contacts', [LeadController::class, 'store']);
